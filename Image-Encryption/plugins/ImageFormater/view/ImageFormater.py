21771af05ae166dbc9661ba57b9ac9db3755081f
havarjos <josquin.havard@etu.univ-rouen.fr>
2017-01-19 12:10:10 +0100

    Modification de Image Formatter

--- Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
+++ Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
@@ -35,13 +35,18 @@ class ImageFormater(Frame):
         '''self.saveButton = tkinter.Button(self.frame,"Sauvegarder",cmd = self.saveCmd)'''
         self._loadButton = tkinter.Button(self,text= "Charger",command = self._loadCmd)
         self._loadButton.pack()
+        self._canvas = tkinter.Canvas(self,width = 500,height = 500, bg = 'blue')
+        self._canvas.pack()
     
     '''
     Création des controlleurs
     '''
     def _loadCmd(self):
-        pass
-        #image_path = tkinter.filedialog.askopenfilename(title = "Ouvrir une image",
-                                                        #filetypes = [('ppm files','.ppm')])
-        #image = tkinter.filedialog.PhotoImage(file = image_path) 
+        
+        picture_path = tkinter.filedialog.askopenfilename(title = "Ouvrir une image",
+                                                        filetypes = [('gif files','.gif')])
+        picture = tkinter.filedialog.PhotoImage(file = picture_path) 
+        self._canvas.create_image(0,0,anchor = CENTER,image = picture )
+        self._canvas.pack()
+        
                     
