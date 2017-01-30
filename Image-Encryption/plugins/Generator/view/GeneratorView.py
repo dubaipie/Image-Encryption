8852c0808bc6f3fe790b13e4783487c94b988af0
Girot <Girot@LAPTOP-35U6568M>
2017-01-30 21:17:54 +0100

    Modification de la vue du générateur avec des scrollbar

--- Image-Encryption/plugins/Generator/view/GeneratorView.py
+++ Image-Encryption/plugins/Generator/view/GeneratorView.py
@@ -1,6 +1,6 @@
-from tkinter import *
 from PIL import Image
-'''Ajouter l'importation ImageTk à PIL'''
+import PIL
+from PIL import ImageTk
 from tkinter.messagebox import *
 from tkinter.filedialog import *
 from Generator.model import GeneratorModel
@@ -27,23 +27,33 @@ class GeneratorView(Frame):
         self._width = Entry(self._frame1, textvariable="", width=5)
         self._height = Entry(self._frame1, textvariable="", width=5)
         
-        self._bouton_generer = Button(self, text=_("Generate"))
-        self._bouton_saveas = Button(self, text=_("Save as ..."))
+        self._bouton_generer = Button(self, text = "Générer")
+        self._bouton_saveas = Button(self, text = "Enregistrer sous")
         self._image = Canvas(self, bg='ivory')
         
+        # horizontal scrollbar
+        self._hbar = Scrollbar(self, orient=HORIZONTAL)
+         
+        # vertical scrollbar
+        self._vbar = Scrollbar(self, orient=VERTICAL)
+         
+        
     def placeComponents(self):    
-        label = Label(self._frame1, text=_("Size :"))
+        label = Label(self._frame1, text = "Taille :")
         label.grid(row = 1, column = 1)
-        label = Label(self._frame1, text = _(" x "))
+        label = Label(self._frame1, text = " x ")
         label.grid(row = 1, column = 3)
         self._width.grid(row = 1, column = 2)
         self._height.grid(row = 1, column = 4)
         
         self._frame1.grid(row = 1, column = 1, columnspan = 4)
-        
-        self._image.grid(row = 3, columnspan = 4, column = 1, sticky = W + E + N + S, padx = 5)
         self._bouton_generer.grid(row = 2, columnspan = 4, column = 1, sticky = W + E)
-        self._bouton_saveas.grid(row = 4, column = 1, columnspan = 4, sticky = W + E)
+        
+        self._image.grid(row = 3, columnspan = 4, column = 1, padx = 5)
+        self._vbar.grid(row=3, column=5, sticky=NW+SE)
+        
+        self._hbar.grid(row=4, column=1, sticky=NW+SE)
+        self._bouton_saveas.grid(row = 5, column = 1, columnspan = 4, sticky = W + E)
         
     def createController(self):
         self._bouton_generer.config(command = self._genererCommand)
@@ -51,34 +61,45 @@ class GeneratorView(Frame):
         
         self.grid_columnconfigure(1, weight = 1)
         self.grid_rowconfigure(3, weight = 1)
-    
+        
+        self._image.configure(
+            xscrollcommand=self._hbar.set,
+            yscrollcommand=self._vbar.set
+        )
+        self._hbar.configure(command=self._image.xview)
+        self._vbar.configure(command=self._image.yview)
+        
     def _saveas(self):
-        repfic = asksaveasfilename(title=_("Save as"), defaultextension=".ppm")
+        repfic = asksaveasfilename(title="Enregistrer sous", defaultextension=".ppm") 
         if len(repfic) > 0:
             try:
                 self._model.getKey().save(repfic)
+                showinfo("Sauvegarde", "La clé à été enregistrée sous :" + repfic)
             except AttributeError:
-                showerror(_("Saving Error"), _("Please generate a key before saving"))
+                showerror("Sauvegarde", "Veuillez générer une clé avant de sauvegarder")
             except KeyError:
-                showerror(_("Saving Error"), _("Saving failed, please check your file name"))
+                showerror("Sauvegarde", "Sauvegarde échoué veuillez vérifier que le nom du fichier est correct")
         
     def _genererCommand(self):
         try:
             w = int (self._width.get())
             h = int (self._height.get())
             if w < 0 or h < 0 or w % 2 != 0 or h % 2 != 0:
-                showerror(_("Generator"), _("Please type even values"))
+                showerror("Générateur", "Veuillez entrer des entiers pair")
             else:
                 self._model.setSize(int (self._width.get()), int (self._height.get()))
                 self._model.generatorKey()
+                '''Gestion de l'affichage de l'image dans le canvas, le canvas prend la taille de l'image '''
+                self._model.getKey().save("tmp_image.ppm")
+                monimage = "tmp_image.ppm"
+                photo = ImageTk.PhotoImage(file = monimage)
+                im = PIL.Image.open(monimage)
+                x, y = im.size
+                im.close()
+                self._image.config(width = x, height = y)
+                self._image.config(scrollregion=(0,0,x,y))
+                self._image.create_image(x / 2, y / 2, image = photo)
+                self._image.image = photo
+                os.remove("tmp_image.ppm")
         except ValueError:
-            showerror(_("Generator"), _("Please type integer values"))
-        '''
-         Insertion de l'image après génération besoin de ImageTk à modifier
-        
-        self._model.getKey().save("tmp_image")
-        monimage = Image.open("tmp_image")
-        photo = ImageTk.PhotoImage(monimage) 
-        self._image.create_image(85, 85, image = photo)
-        os.remove('tmp_image')
-        '''
+            showerror("Générateur", "Veuillez entrer des décimaux")
