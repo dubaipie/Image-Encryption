7aaeab0cdbd881a4524ad6b4317fe712bb7cbdec
Pierre DUBAILLAY <pierre.dubaillay@orange.fr>
2017-01-31 21:52:34 +0100

    Création de ImageFormaterModel et ImageFormater

--- Image-Encryption/plugins/ImageFormater/model/ImageFormaterModel.py
+++ Image-Encryption/plugins/ImageFormater/model/ImageFormaterModel.py
@@ -4,11 +4,18 @@ Created on 18 janv. 2017
 @author: havarjos
 '''
 
-from PIL import ImageTk
-from builtins import IOError
+from PIL import Image
+
+
+class NoImageToConvert(Exception):
+    """
+    Exception levée quand aucune image à convertir n'a été trouvée.
+    """
+    pass
+
 
 class ImageFormaterModel(object):
-    
+    """
     def setImageToConvert(self, img):
         self._image_to_convert = img
     
@@ -86,4 +93,57 @@ class ImageFormaterModel(object):
             self.getImageConvert().save(path_file,'PPM')
         except IOError:
             print ("Probleme de lecture/écritue")
-            raise IOError                               
+            raise IOError
+    """
+    def __init__(self):
+        """
+        Constructeur de convertisseur d'image.
+        """
+        self._origin = None
+        self._converted = None
+
+    @property
+    def originalPicture(self):
+        """
+        Donne l'image d'origine.
+        :return: une instance de PIL.Image ou None
+        """
+        return self._origin
+
+    @originalPicture.setter
+    def originalPicture(self, path_or_image):
+        """
+        Permet de spécifier l'image qui sera convertie lors d'un appel à convert()
+        :param path_or_image: le chemin vers l'image ou bien l'immage elle-même.
+        :raise TypeError: le type ne permet pas le chargement de l'image.
+        :raise IOError: le chemin ne pointe pas sur une image valide.
+        """
+        if type(path_or_image) is str:
+            self._origin = Image.open(path_or_image)
+        elif type(path_or_image) is Image:
+            self._origin = path_or_image
+        else:
+            raise TypeError
+
+    def convert(self):
+        """
+        Convertit l'image originale en sa transformée, 2x plus grande.
+        :raise NoImageToConvert: s'il n'y a pas d'image originale.
+        """
+        if self._origin is None:
+            raise NoImageToConvert
+
+        self._converted = self._origin.convert('1')
+        width, height = self._converted.width, self._converted.height
+        width *= 2
+        height *= 2
+        self._converted = self._converted.resize((width, height), Image.BILINEAR)
+
+    @property
+    def convertedPicture(self):
+        """
+        Donne l'image convertie si un appel avec succès à convert() à eu lieu,
+        None sinon.
+        :return: L'image convertie ou None
+        """
+        return self._converted
--- Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
+++ Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
@@ -3,18 +3,19 @@ Created on 18 janv. 2017
 
 @author: havarjos
 '''
-import tkinter
+from tkinter import Frame, Entry, Label, Canvas, Button, Scrollbar
+from tkinter import StringVar, HORIZONTAL, VERTICAL
+from tkinter import filedialog, messagebox
 from PIL import ImageTk
-import ImageFormater.model.ImageFormaterModel as IFM
-from tkinter.filedialog import *
-from tkinter import messagebox
-import tkinter.ttk as ttk
+from ImageFormater.model.ImageFormaterModel import *
+
 
 class ImageFormater(Frame):
     '''
     La vue du modèle ImageFormaterModel
     '''  
     
+    """
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.createModel()
@@ -67,8 +68,127 @@ class ImageFormater(Frame):
         else:
             self._model.upImageResolution()    
             
-                
-          
-        
-          
-                    
+    """
+    FORMATS = (
+        ("Bitmap", "*.bmp"),
+        ("Encapsulated PostScript", "*.eps"),
+        ("Graphics Interchange Format", "*.gif"),
+        ("Joint Photographic Experts Group", "*.jpg, *.jpeg"),
+        ("Portable Network Graphics", "*.png"),
+        ("Portable pixmap", " *.ppm, *.pgm, *.pbm"),
+        ("All file", "*.*")
+    )
+
+    def __init__(self, master=None):
+        """
+        Le constructeur de la fenêtre.
+        :param master: le parent dans la hiérarchie d'affichage.
+        """
+        Frame.__init__(self, master)
+
+        self._createModel()
+        self._createView()
+        self._placeComponents()
+        self._createController()
+
+    def _createModel(self):
+        """
+        Création du modèle associé à la vue.
+        """
+        self._model = ImageFormaterModel()
+
+        self._originalStrVar = StringVar()
+        self._convertedStrVar = StringVar()
+
+    def _createView(self):
+        """
+        Création des éléments composant la vue.
+        """
+        self._originalEntry = Entry(self)
+        self._originalEntry.config(state="readonly", textvariable=self._originalStrVar)
+
+        self._convertedEntry = Entry(self)
+        self._convertedEntry.config(state="readonly", textvariable=self._convertedStrVar)
+
+        self._originalButton = Button(self, text="Parcourir")
+        self._convertedButton = Button(self, text="Parcourir")
+
+        self._canvas = Canvas(self)
+
+        self._convertButton = Button(self, text="Convertir")
+
+        self._hbar = Scrollbar(self, orient=HORIZONTAL)
+        self._vbar = Scrollbar(self, orient=VERTICAL)
+
+    def _placeComponents(self):
+        """
+        Permet de placer les composants dans la vue.
+        """
+        Label(self, text="Image originale").grid(row=1, column=1)
+        self._originalEntry.grid(row=1, column=2)
+        self._originalButton.grid(row=1, column=3)
+
+        Label(self, text="Image convertie").grid(row=2, column=1)
+        self._convertedEntry.grid(row=2, column=2)
+        self._convertedButton.grid(row=2, column=3)
+
+        self._convertButton.grid(row=3, column=2, columnspan=2)
+
+        self._canvas.grid(row=1, rowspan=3, column=4)
+
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+
+    def _createController(self):
+        """
+        Création des contrôleurs de la vue et du model.
+        """
+        self._originalButton.config(command=self._onOriginalButtonClick)
+        self._convertedButton.config(command=self._onConvertedButtonClick)
+        self._convertButton.config(command=self._onConvertButtonClick)
+
+        self._canvas.config(xscrollcommand=self._hbar, yscrollcommand=self._vbar)
+        self._hbar.config(command=self._canvas.xview)
+        self._vbar.config(command=self._canvas.yview)
+
+    def _onOriginalButtonClick(self):
+        """
+        Action déclenchée lors du clique sur le bouton originalButton
+        """
+        dlg = filedialog.askopenfilename(filetypes=ImageFormater.FORMATS)
+
+        if dlg != "":
+            try:
+                self._model.originalPicture = dlg
+                self._originalStrVar.set(dlg)
+            except NoImageToConvert:
+                messagebox.showerror("Erreur",
+                                     "Entrez un chemin vers une image valide")
+            except IOError:
+                messagebox.showerror("Erreur",
+                                     "Erreur lors de l'ouerture de l'image")
+
+    def _onConvertedButtonClick(self):
+        """
+        Action déclenchée lors du clique sur le bouton convertedButton
+        """
+        dlg = filedialog.asksaveasfilename(defaultextension=".ppm")
+
+        if dlg != "":
+            self._convertedStrVar.set(dlg)
+
+    def _onConvertButtonClick(self):
+        """
+        Action effectuée lors du clique sur le bouton convertButton
+        """
+        if self._model.originalPicture is not None and self._convertedStrVar is not None:
+            self._model.convert()
+            picture = self._model.convertedPicture
+            picture.save(self._convertedStrVar.get())
+            picture = ImageTk.PhotoImage(picture)
+            w, h = picture.width(), picture.height()
+            self._canvas.config(width=w,
+                                height=h,
+                                scrollregion=(0, 0, w, h))
+            self._canvas.create_image(w / 2, h / 2, image=picture)
+            self._canvas.picture = picture
