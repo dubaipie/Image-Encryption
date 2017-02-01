aea0ed8a81fc9cbb377802e50d14df669913f545
havarjos <josquin.havard@etu.univ-rouen.fr>
2017-02-01 16:11:44 +0100

    Modification de la vue du Generator et suppression des commentaires dans ImageFormater et ImageFormaterModel

--- Image-Encryption/plugins/Generator/view/GeneratorView.py
+++ Image-Encryption/plugins/Generator/view/GeneratorView.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import PIL
 from PIL import ImageTk
+from tkinter import filedialog
 from tkinter.messagebox import *
 from tkinter.filedialog import *
 from Generator.model import GeneratorModel
 
 class GeneratorView(Frame):
+    
     def __init__(self, master=None):
         '''
         Constructeur de la fenêtre.
@@ -21,14 +23,23 @@ class GeneratorView(Frame):
         Initialisation du model.
         '''
         self._model = GeneratorModel.GeneratorModel()
+        
+        self._imgVar = StringVar()
+        self._widthVar = StringVar()
+        self._heightVar = StringVar()
+        self._keyWidth = 0
+        self._keyHeight = 0
     
     def createView(self):
         self._frame1 = Frame(self)
-        self._width = Entry(self._frame1, textvariable="", width=5)
-        self._height = Entry(self._frame1, textvariable="", width=5)
+        self._width = Entry(self._frame1, textvariable=self._widthVar, width=5)
+        self._height = Entry(self._frame1, textvariable=self._heightVar, width=5)
+        self._imgEntry = Entry(self._frame1,textvariable=self._imgVar) 
         
         self._bouton_generer = Button(self, text = "Générer")
         self._bouton_saveas = Button(self, text = "Enregistrer sous")
+        self._bouton_setSize1 = Button(self._frame1, text = "Ok")
+        self._bouton_setSize2 = Button(self._frame1, text = "Parcourir")
         self._image = Canvas(self, bg='ivory')
         
         # horizontal scrollbar
@@ -45,6 +56,13 @@ class GeneratorView(Frame):
         label.grid(row = 1, column = 3)
         self._width.grid(row = 1, column = 2)
         self._height.grid(row = 1, column = 4)
+        self._bouton_setSize1.grid(row = 1, columnspan = 1 ,column = 5,sticky = W + E)
+        label = Label(self._frame1, text = "          ")
+        label.grid(row = 1, column = 6)
+        label = Label(self._frame1, text = "Choisir une taille à partir d'une image : ")
+        label.grid(row = 1, column = 7)
+        self._imgEntry.grid(row = 1, column = 8)
+        self._bouton_setSize2.grid(row = 1, columnspan = 1, column = 9, sticky = W + E)
         
         self._frame1.grid(row = 1, column = 1, columnspan = 4)
         self._bouton_generer.grid(row = 2, columnspan = 4, column = 1, sticky = W + E)
@@ -58,6 +76,8 @@ class GeneratorView(Frame):
     def createController(self):
         self._bouton_generer.config(command = self._genererCommand)
         self._bouton_saveas.config(command = self._saveas)
+        self._bouton_setSize1.config(command = self._setSizeWithNumber)
+        self._bouton_setSize2.config(command = self._setSizeWithImage)
         
         self.grid_columnconfigure(1, weight = 1)
         self.grid_rowconfigure(3, weight = 1)
@@ -82,8 +102,8 @@ class GeneratorView(Frame):
         
     def _genererCommand(self):
         try:
-            w = int (self._width.get())
-            h = int (self._height.get())
+            w = self._keyWidth
+            h = self._keyHeight
             if w < 0 or h < 0 or w % 2 != 0 or h % 2 != 0:
                 showerror("Générateur", "Veuillez entrer des entiers pair")
             else:
@@ -103,3 +123,26 @@ class GeneratorView(Frame):
                 os.remove("tmp_image.ppm")
         except ValueError:
             showerror("Générateur", "Veuillez entrer des décimaux")
+    
+    def _setSizeWithNumber(self):
+        w = int (self._width.get())
+        h = int (self._height.get())
+        
+        if w < 0 or h < 0 or w % 2 != 0 or h % 2 != 0:
+            showerror("Générateur", "Veuillez entrer des entiers pair")
+        else:
+            self._keyWidth = w
+            self._keyHeight = h
+                   
+    def _setSizeWithImage(self):
+        dlg = filedialog.askopenfilename(title="Ouvrir", filetypes=[("PPM", "*.ppm")])
+        if dlg != "":
+            self._imgVar.set(dlg)
+            im = PIL.Image.open(dlg) 
+            self._keyWidth = int (im.width)
+            self._keyHeight = int (im.height)
+            im.close()     
+            self._widthVar.set(self._keyWidth)
+            self._heightVar.set(self._keyHeight)
+        
+                
--- Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
+++ Image-Encryption/plugins/ImageFormater/view/ImageFormater.py
@@ -11,64 +11,6 @@ from ImageFormater.model.ImageFormaterModel import *
 
 
 class ImageFormater(Frame):
-    '''
-    La vue du modèle ImageFormaterModel
-    '''  
-    
-    """
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.createModel()
-        self.createView()
-        'self.placeComponents()'
-        #self.creteController() 
-    
-    
-    '''
-    Création du modèle
-    '''        
-    def createModel(self):
-        self._model = IFM.ImageFormaterModel()
-    
-    '''
-    Création des composants graphiques
-    '''
-    
-    def createView(self):
-        #self.saveButton = tkinter.Button(self,"Sauvegarder",command = self._saveCmd)
-        #self._loadButton = tkinter.Button(self,text= "Charger",command = self._loadCmd)
-        #self._loadButton.pack()
-        #self._canvas = tkinter.Canvas(self,width = 500,height = 500, bg = 'blue')
-        #self._canvas.pack()
-        #self._resolutionButton(self,"Changer la résolution",command = self._changeResolution)
-        pass
-    
-    '''
-    Création des controlleurs
-    '''
-    def _loadCmd(self):
-        
-        picture_path = tkinter.filedialog.askopenfilename(title = "Ouvrir une image",
-                                                            filetypes = [('gif files','.gif'),('ppm files','.ppm')])
-        picture = tkinter.filedialog.PhotoImage(file = picture_path) 
-        self._canvas.create_image(0,0,anchor = CENTER,image = picture )
-        self._canvas.image = picture
-        self._canvas.pack()
-        self._model.changeImageToConvert(picture_path)
-    
-    def _saveCmd(self):
-        picture_path = tkinter.filedialog.askopenfilename(title = "Enregistrer sous",
-                                                            filetypes = [('gif files','.gif'),('ppm files','.ppm')])
-        picture = tkinter.filedialog.PhotoImage(file = picture_path)
-        self._model.saveImageConvert(picture)
-    
-    def _changeResolution(self):
-        if self._model.getImageToConvert() == None:
-            messagebox.showerror("Erreur","Impossible de trouver l'image à convertir")
-        else:
-            self._model.upImageResolution()    
-            
-    """
     FORMATS = [
         ("Bitmap", "*.bmp"),
         ("Encapsulated PostScript", "*.eps"),
